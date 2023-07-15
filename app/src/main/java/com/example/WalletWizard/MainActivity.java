70caddfe1045ba37bd16fe5f81c0bdec94e4f70b
PrajwalRana <prajwalrana07@gmail.com>
2023-07-15 02:08:09 -0500

    this

--- app/src/main/java/com/example/WalletWizard/MainActivity.java
+++ app/src/main/java/com/example/WalletWizard/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.WalletWizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
