8b54a196dce6aead17645425d98b4403620b6996
PrajwalRana <prajwalrana07@gmail.com>
2023-07-15 17:37:52 -0500

    display user input

--- app/src/main/java/com/example/WalletWizard/MainActivity.java
+++ app/src/main/java/com/example/WalletWizard/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.WalletWizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button login = (Button) findViewById(R.id.signinbutton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, username.getText().toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, password.getText().toString(),Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        }
 
 
 
 }
+
+
+
+
